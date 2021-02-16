fe9d2049b25352c320dfae850e6b80b2928cb0f3
Akif Dere <dereakif@gmail.com>
2021-02-17 00:14:09 +0300

    HackerRank Solutions

--- Day6-Bitwise-Operators.js
+++ Day6-Bitwise-Operators.js
@@ -1,9 +1,9 @@
 function getMaxLessThanK(n, k) {
   let max = 0;
-
+  let bitwise;
   for (let a = 1; a <= n; a++) {
     for (let b = a + 1; b <= n; b++) {
-      let bitwise = a & b;
+      bitwise = a & b;
       if (bitwise < k && bitwise > max) {
         max = bitwise;
       }
