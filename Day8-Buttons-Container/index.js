6b50c2ab4a48e96b587007e542253a3aa78fb9a8
Akif Dere <dereakif@gmail.com>
2021-02-18 21:36:32 +0300

    HackerRank Solutions

--- Day8-Buttons-Container/index.js
+++ Day8-Buttons-Container/index.js
@@ -1,4 +1,4 @@
-const ids = [1, 2, 3, 6, 9, 8, 7, 4]; // start positions ids in clockwise order
+const ids = [1, 2, 3, 6, 9, 8, 7, 4];
 let nums = [1, 2, 3, 6, 9, 8, 7, 4];
 document.getElementById("btn5").addEventListener("click", () => {
   nums.unshift(nums.pop());
